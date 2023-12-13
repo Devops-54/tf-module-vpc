e013e83fb72505f196af02c2818d9acbcae875aa
Uttej uv <sushprav11@gmail.com>
2023-12-13 19:23:33 +0530

    AutoPush

--- subnets.tf
+++ subnets.tf
@@ -14,7 +14,6 @@ resource "aws_subnet" "private_subnet" {
 
   vpc_id                    = aws_vpc.main.id
   cidr_block                = element(var.PRIVATE_SUBNET_CIDR, count.index)
-  availability_zone         = element(var.AZ, count.index)
 
   tags = {
     Name                    = "roboshop-${var.ENV}-private-subnet-${element(var.AZ, count.index)}"
