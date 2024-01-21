c9ea0d3ebea80ade88a3e42fb479f14dff0a48db
Uttej uv <sushprav11@gmail.com>
2024-01-21 15:13:03 +0530

    Auto push

--- route-table.tf
+++ route-table.tf
@@ -7,6 +7,11 @@ resource "aws_route_table" "public_rt" {
     gateway_id                = aws_internet_gateway.igw.id
   }
 
+  route {
+    cidr_block                = var.DEFAULT_VPC_CIDR
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  }
+
   tags = {
     Name = "roboshop-${var.ENV}-public-rt"
   }
@@ -25,9 +30,14 @@ resource "aws_route_table_association" "public_subnet_rt_association" {
   vpc_id                = aws_vpc.main.id
 
  route {
-    cidr_block          = "0.0.0.0/0"
-    nat_gateway_id      = aws_nat_gateway.ngw.id
-}
+   cidr_block          = "0.0.0.0/0"
+   nat_gateway_id      = aws_nat_gateway.ngw.id
+ }
+
+ route {
+   cidr_block                = var.DEFAULT_VPC_CIDR
+   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+ }
 
   tags = {
     Name = "roboshop-${var.ENV}-private-rt"
